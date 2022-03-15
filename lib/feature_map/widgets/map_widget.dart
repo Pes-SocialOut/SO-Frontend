0daf512427d4ddbb2eb456363c197a848aeccc75
Guillem García <guillem.garcia.gil@protonmail.com>
2022-03-15 17:07:22 +0100

    Flutter map template

---
+++ lib/feature_map/widgets/map_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/plugin_api.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapWidget extends StatefulWidget {
+  const MapWidget({ Key? key }) : super(key: key);
+
+  @override
+  State<MapWidget> createState() => _MapWidgetState();
+}
+
+class _MapWidgetState extends State<MapWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(41.3879, 2.16992),
+        zoom: 15,
+      ),
+      layers: [
+        TileLayerOptions(
+          urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+          subdomains: ['a', 'b', 'c'],
+        )
+      ],
+    );
+  }
+}
