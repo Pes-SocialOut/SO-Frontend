d4465315013e1c86b5d0b6f7dc05c05cb646de40
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-03-23 17:52:18 +0100

    App name changed to SocialOut

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SocialOut',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme:ColorScheme(
