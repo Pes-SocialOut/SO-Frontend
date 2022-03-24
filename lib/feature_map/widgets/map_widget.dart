7968ed1e3ee0eeea934dea9a2106aba7fa29695c
Guillem García <guillem.garcia.gil@protonmail.com>
2022-03-24 16:52:25 +0100

    Event widget integration

--- lib/feature_map/widgets/map_widget.dart
+++ lib/feature_map/widgets/map_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:so_frontend/feature_map/widgets/map_event.dart';
 
 class MapWidget extends StatefulWidget {
   const MapWidget({ Key? key }) : super(key: key);
@@ -14,12 +15,23 @@ class _MapWidgetState extends State<MapWidget> {
   final double lat = 41.3879;
   final double long = 2.16992;
   
+  showEvent() {
+    showModalBottomSheet(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0)
+        ),
+        context: context,
+        builder: (BuildContext context) {
+          return const EventWidget();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
       options: MapOptions(
         center: LatLng(lat, long),
-        zoom: 13,
+        zoom: 10,
       ),
       layers: [
         TileLayerOptions(
@@ -33,11 +45,21 @@ class _MapWidgetState extends State<MapWidget> {
             height: 40.0,
             point: LatLng(lat, long),
             builder: (context) =>  Icon(
-              Icons.circle_outlined,
+              Icons.circle,
               color: Theme.of(context).colorScheme.primary,
               size: 40,
             )
           ),
+          Marker(
+            width: 40.0,
+            height: 40.0,
+            point: LatLng(lat + 0.1, long + 0.1),
+            builder: (context) =>  IconButton(
+              icon: const Icon(Icons.location_on_sharp, size: 40),
+              color:Color.fromARGB(255, 205, 193, 93),
+              onPressed: showEvent
+            )
+          )
         ],
       ),
       ],
