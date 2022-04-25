2d98e699b0169fede365dad879278e261945b4b4
WeijieLiu1 <weijie.liu@estudiantat.upc.edu>
2022-04-26 01:33:02 +0200

    welcome_screeen

--- lib/feature_user/screens/welcome_screen.dart
+++ lib/feature_user/screens/welcome_screen.dart
@@ -1,42 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:so_frontend/utils/theme.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Welcome'),
-            const SizedBox(height:20),
-            InkWell(
-              onTap: () {
-                Navigator.of(context).pushNamed('/login');
-              },
-              child: const SizedBox(
-                width: 400,
-                height: 100,
-                child: Icon(Icons.login),
-              )
-            ),
-            const SizedBox(height:20),
-            InkWell(
-              onTap: () {
-                Navigator.of(context).pushNamed('/signup');
-              },
-              child: const SizedBox(
-                width: 400,
-                height: 100,
-                child: Icon(Icons.add),
-              )
-            )
-          ]
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "assets/frontpage/frontpage_1.png",
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
         ),
-      )
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              //mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height:80),
+                Image.asset(
+                  "assets/Banner.png",
+                  height: MediaQuery.of(context).size.height/10,
+                  width: MediaQuery.of(context).size.width/1.5,
+                  
+                ),
+                const SizedBox(height:50),
+                const Text(
+                  'Enjoy the day outside',
+                  style: TextStyle(height: 1.0, fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height:200),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color(0x0038a3a5),//const Color(0x0038a3a5),
+                    onPrimary: Colors.white,
+
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/login');
+                  },
+                  
+                  child: const Text(
+                    
+                    'Sign Up',
+                    style: TextStyle(height: 1.0, fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(height:20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color(0x0022577a),
+                    onPrimary: Colors.black,
+
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/signup');
+                  },
+                  child: const Text(
+                    'Log In',
+                    style: TextStyle(height: 1.0, fontSize: 30,  fontWeight: FontWeight.bold),
+                  ),
+                )
+              ]
+            ),
+          )
+        )
+      ],
     );
+    
   }
 }
