9be325fffd921d97a692b79a8c9c4d45ecaf3e18
lauv0x <laura.alvarez.berrocal@estudiantat.upc.edu>
2022-04-26 16:18:30 +0200

    .

--- lib/feature_home/screens/home.dart
+++ lib/feature_home/screens/home.dart
@@ -2,33 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:so_frontend/feature_home/widgets/map_button.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('SocialOut', style: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Theme.of(context).colorScheme.secondary,
+        appBar: AppBar(
+          title: Text('SocialOut',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.secondary,
+                backgroundColor: Theme.of(context).colorScheme.background,
+              )),
+          elevation: 1,
           backgroundColor: Theme.of(context).colorScheme.background,
-        )),
-        elevation: 1,
-        backgroundColor: Theme.of(context).colorScheme.background,
-      ),
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        margin: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Text('Search your near events',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-            MapButton()
-          ],
+          leading: IconButton(
+              icon: const Icon(
+                Icons.account_circle_sharp,
+                color: Colors.black,
+              ),
+              onPressed: () {}),
         ),
-      )
-    );
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: Container(
+          margin: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                'Search your near events',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              MapButton()
+            ],
+          ),
+        ));
   }
 }
--- lib/feature_map/screens/map.dart
+++ lib/feature_map/screens/map.dart
@@ -4,20 +4,19 @@ import 'package:so_frontend/feature_map/services/geolocation.dart';
 import 'package:geolocator/geolocator.dart';
 
 class MapScreen extends StatefulWidget {
-  const MapScreen({ Key? key }) : super(key: key);
+  const MapScreen({Key? key}) : super(key: key);
 
   @override
   State<MapScreen> createState() => MapScreenState();
 }
 
 class MapScreenState extends State<MapScreen> {
-  
   double lat = 0;
   double long = 0;
   GeolocationService gs = GeolocationService();
 
-  assignLocation () async {
-    List coords = await gs.getLocation(); 
+  assignLocation() async {
+    List coords = await gs.getLocation();
     setState(() {
       lat = coords[0];
       long = coords[1];
@@ -28,41 +27,29 @@ class MapScreenState extends State<MapScreen> {
   void initState() {
     super.initState();
     assignLocation();
-    
   }
 
   @override
   Widget build(BuildContext context) {
-    
-
-    return  Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Stack(
-        children: [
+    return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: Stack(children: [
           MapWidget(lat: lat, long: long),
           Padding(
-            padding: const EdgeInsets.only(
-              top: 40,
-              left: 16
-            ),
+            padding: const EdgeInsets.only(top: 40, left: 16),
             child: CircleAvatar(
               radius: 22,
               backgroundColor: Theme.of(context).colorScheme.background,
               child: IconButton(
-                icon: Icon(
-                  Icons.arrow_back_ios_new_rounded,
-                  size: 22,
-                  color: Theme.of(context).colorScheme.onBackground
-                ),
+                icon: Icon(Icons.arrow_back_ios_new_rounded,
+                    size: 22,
+                    color: Theme.of(context).colorScheme.onBackground),
                 onPressed: () {
                   Navigator.pop(context);
                 },
               ),
             ),
           )
-          
-        ]
-      )
-    );
+        ]));
   }
 }
