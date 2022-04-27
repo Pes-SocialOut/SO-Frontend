993f84a97ec88f9e6ba164e9f07f59b1154443ee
WeijieLiu1 <weijie.liu@estudiantat.upc.edu>
2022-04-27 15:31:11 +0200

    screens of user done

--- lib/feature_user/screens/login_screen.dart
+++ lib/feature_user/screens/login_screen.dart
@@ -20,111 +20,148 @@ class LoginScreen extends StatelessWidget {
       ),
       body: Center(
         
-        child: Column(
-          
-          crossAxisAlignment: CrossAxisAlignment.center,
-          //mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SignInButton(
-              Buttons.Google,
-              
-              text: "Sign up with Google",
-              onPressed: () {},
-            ),
-            SignInButton(
-              Buttons.Facebook,
-              text: "Sign up with Facebook",
-              onPressed: () {},
-            ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                hintText: "Enter email",
-                labelText: "Username"
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: SignInButton(
+                  Buttons.Google,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderradius)),
+                  text: "Sign up with Google",
+                  onPressed: () {},
+                ),
               ),
-            ),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: "Enter password",
-                labelText: "Password"
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: SignInButton(
+                  Buttons.Facebook,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderradius)),
+                  text: "Sign up with Facebook",
+                  onPressed: () {},
+                ),
               ),
-            ),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    hintText: "Enter email",
+                    labelText: "Username"
+                  ),
+                ),
+              ),
 
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Theme.of(context).colorScheme.secondary,
-                onPrimary: Colors.white,
-                shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(borderradius)),
-                minimumSize: Size(widthButton, heightButton)
-              ),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/home');
-              },
-              
-              child: const Text(
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
                 
-                'Log In',
-                style: TextStyle(height: 1.0, fontSize: 20, fontWeight: FontWeight.bold),
+                child: TextFormField(
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: "Enter password",
+                    labelText: "Password"
+                  ),
+                ),
               ),
-            ),
-            InkWell(
-              child: new Text(
-                'Forget password?',
-                style: TextStyle(color: Theme.of(context).colorScheme.secondary, fontSize: policyTextSize),
-                ),
-              onTap:  () async {
-              final Uri uri = Uri(scheme: 'https', host: 'www.github.com');
-              await launchUrl(uri);
-              //cant launch at the moment, because emualtor has no internet
-              /*
-              if(await canLaunchUrl(url)){
-                await launchUrl(url);
-              }
-              else{
-                throw "cannot load Url";
-              }
-              */
-              }
-            ),
-            RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    style: TextStyle(color: Theme.of(context).colorScheme.surface, fontSize: policyTextSize),
-                    text: "Don't haven an account? "
+              
+
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child:  ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Theme.of(context).colorScheme.secondary,
+                    onPrimary: Colors.white,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderradius)),
+                    minimumSize: Size(widthButton, heightButton)
                   ),
-                  TextSpan(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/home');
+                  },
+                  
+                  child: const Text(
+                    
+                    'Log In',
+                    style: TextStyle(height: 1.0, fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+             
+
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: InkWell(
+                  child: Text(
+                    'Forget password?',
                     style: TextStyle(color: Theme.of(context).colorScheme.secondary, fontSize: policyTextSize),
-                    text: "Sign up",
-                    recognizer: TapGestureRecognizer()..onTap = () {
-                      Navigator.of(context).pushNamed('/signup');
-                      //cant launch at the moment, because emualtor has no internet
-                      /*
-                      if(await canLaunchUrl(url)){
-                        await launchUrl(url);
-                      }
-                      else{
-                        throw "cannot load Url";
-                      }
-                      */
-                    }
-                  ),
-                ]
+                    ),
+                  onTap:  () async {
+                    final Uri uri = Uri(scheme: 'https', host: 'www.github.com');
+                    await launchUrl(uri);
+                  //cant launch at the moment, because emualtor has no internet
+                  /*
+                  if(await canLaunchUrl(url)){
+                    await launchUrl(url);
+                  }
+                  else{
+                    throw "cannot load Url";
+                  }
+                  */
+                  }
                 ),
-            ),
-            const SizedBox(height:100),
+              ),
+              
+
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        style: TextStyle(color: Theme.of(context).colorScheme.surface, fontSize: policyTextSize),
+                        text: "Don't haven an account? "
+                      ),
+                      TextSpan(
+                        style: TextStyle(color: Theme.of(context).colorScheme.secondary, fontSize: policyTextSize),
+                        text: "Sign up",
+                        recognizer: TapGestureRecognizer()..onTap = () {
+                          Navigator.of(context).pushNamed('/signup');
+                          //cant launch at the moment, because emualtor has no internet
+                          /*
+                          if(await canLaunchUrl(url)){
+                            await launchUrl(url);
+                          }
+                          else{
+                            throw "cannot load Url";
+                          }
+                          */
+                        }
+                      ),
+                    ]
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const PolicyWidget(),
+              ),              
+              
             
-            Container(
-              constraints: BoxConstraints(minWidth: 350,maxWidth:350),
-              padding: EdgeInsets.all(10),
-              child: PolicyWidget(),
-            ),
             
-            
-          ] 
+            ] 
+          )
+          
+          
         ),
         
       )
--- lib/feature_user/screens/signup_screen.dart
+++ lib/feature_user/screens/signup_screen.dart
@@ -1,33 +1,119 @@
+
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:so_frontend/feature_user/widgets/policy.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double borderradius = 10.0;
+    double widthButton = 300.0;
+    double heightButton = 40.0;
+    double policyTextSize = 14;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xC8C8C8),
         title: const Text('Sign Up')
       ),
       body: Center(
-        child: Column(
+        
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              const SizedBox(height: 60),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: SignInButton(
+                  Buttons.Google,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderradius)),
+                  text: "Continue with Google",
+                  onPressed: () {},
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: SignInButton(
+                  Buttons.Facebook,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderradius)),
+                  text: "Continue with Facebook",
+                  onPressed: () {},
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  "OR",
+                  style: TextStyle(color: Colors.black45, fontSize: 16),
+                  
+                  ),
+              ), 
+              const SizedBox(height: 10),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: SignInButton(
+                  Buttons.Email,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderradius)),
+                  text: "Continue with Email",
+                  onPressed: () {},
+                ),
+              ),
+                   
+             
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        style: TextStyle(color: Theme.of(context).colorScheme.surface, fontSize: policyTextSize),
+                        text: "Already have an account? "
+                      ),
+                      TextSpan(
+                        style: TextStyle(color: Theme.of(context).colorScheme.secondary, fontSize: policyTextSize),
+                        text: "Log In",
+                        recognizer: TapGestureRecognizer()..onTap = () {
+                          Navigator.of(context).pushNamed('/login');
+                          //cant launch at the moment, because emualtor has no internet
+                          /*
+                          if(await canLaunchUrl(url)){
+                            await launchUrl(url);
+                          }
+                          else{
+                            throw "cannot load Url";
+                          }
+                          */
+                        }
+                      ),
+                    ]
+                  ),
+                ),
+              ),
+              const SizedBox(height: 60),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const PolicyWidget(),
+              ),              
+              
+            
+            
+            ] 
+          )
           
-          //scrossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height:300),
-            Container(
-              constraints: BoxConstraints(minWidth: 350,maxWidth:350),
-              padding: EdgeInsets.all(10),
-              child: PolicyWidget(),
-            ),
-            
-            
-          ] 
+          
         ),
+        
       )
     );
   }
