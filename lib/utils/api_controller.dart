c057e696b04cbf0db6d504efc56978b99619bc07
Francesc Holly <2012.1c.holfrancesc@gmail.com>
2022-05-03 23:29:56 +0200

    task #138: Implement all API methods

--- lib/utils/api_controller.dart
+++ lib/utils/api_controller.dart
@@ -4,113 +4,180 @@ import 'package:collection/collection.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class APICalls {
-    // Seguramente se pueda usar patrón singleton.
-    final String _REFRESH_TOKEN_PREFS = 'socialout_refresh_token';
+  static final APICalls _instance = APICalls._internal();
 
-    final String API_URL = 'https://socialout-develop.herokuapp.com';
-    final String REFRESH_ENDPOINT = '/v1/users/refresh';
-    final UNAUTHORIZED = 401;
+  // Seguramente se pueda usar patrón singleton.
+  final String _REFRESH_TOKEN_PREFS = 'socialout_refresh_token';
 
-    String USER_ID;
-    String ACCESS_TOKEN;
-    String REFRESH_TOKEN;
+  final String API_URL = 'https://socialout-develop.herokuapp.com';
+  final String _REFRESH_ENDPOINT = '/v1/users/refresh';
+  final int _UNAUTHORIZED = 401;
 
-    void initialize(Stirng userId, String accessToken, String refreshToken, bool keepLoginInPreferences) async {
-        USER_ID = userId;
-        ACCESS_TOKEN = accessToken;
-        REFRESH_TOKEN = refreshToken;
+  String _USER_ID = '';
+  String _ACCESS_TOKEN = '';
+  String _REFRESH_TOKEN = '';
 
+  String getCurrentUser() {
+    return _USER_ID;
+  }
+
+  void initialize(String userId, String accessToken, String refreshToken, bool keepLoginInPreferences) async {
+    _USER_ID = userId;
+    _ACCESS_TOKEN = accessToken;
+    _REFRESH_TOKEN = refreshToken;
+
+    if (keepLoginInPreferences) {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString(_REFRESH_TOKEN_PREFS, _REFRESH_TOKEN);
+    }
+  }
+
+  void tryInitializeFromPreferences() async {
+    // Esta función se llama al iniciar la aplicación. Determina si el usuario debe hacer login o si ya "se acuerda".
+    // Leer las preferences, buscar "socialout_refresh". Si no existe redirecciona a la screen de logIn
+    final prefs = await SharedPreferences.getInstance();
+    final String refresh_prefs = prefs.getString(_REFRESH_TOKEN_PREFS);
+    if (refresh_prefs == null) _redirectToLogin();
+    else {
+      // Si hay refresh token, iniciar sesión automáticamente llamando al endpoint de refresh de la API.
+      // Si la operación es aceptada redirecciona a la home screen. Si no redirecciona al logIn.
+      _REFRESH_TOKEN = refresh_prefs;
+      _refresh(() => _redirectToHomeScreen(), () => _redirectToLogin());
+    }
+  }
+
+  void getItem(String endpoint, List<String> pathParams, Function onSuccess, Function onError) async {
+    final uri = _buildUri(endpoint, pathParams, {});
+    final response = await http.get(uri, headers: {
+      'Authorization': 'Bearer $_ACCESS_TOKEN',
+      'Content-Type': 'application/json'
+    });
+    if (response.statusCode == _UNAUTHORIZED) {
+      _refresh(() => getItem(endpoint, pathParams, onSuccess, onError), () => _redirectToLogin());
+    } else if (response.statusCode ~/ 100 == 2) {
+      onSuccess(jsonDecode(response.body));
+    } else {
+      String errorMessage = 'No error message provided';
+      if (jsonDecode(response.body).containsKey('error_message')) errorMessage = jsonDecode(response.body)['error_message'];
+      onError(errorMessage, response.statusCode);
+    }
+  }
+
+  void getCollection(String endpoint, List<String> pathParams, Map<String, String> queryParams, Function onSuccess, Function onError) async {
+    final uri = _buildUri(endpoint, pathParams, queryParams);
+    final response = await http.get(uri, headers: {
+      'Authorization': 'Bearer $_ACCESS_TOKEN',
+      'Content-Type': 'application/json'
+    });
+    if (response.statusCode == _UNAUTHORIZED) {
+      _refresh(() => getCollection(endpoint, pathParams, queryParams, onSuccess, onError), () => _redirectToLogin());
+    } else if (response.statusCode ~/ 100 == 2) {
+      onSuccess(jsonDecode(response.body));
+    } else {
+      String errorMessage = 'No error message provided';
+      if (jsonDecode(response.body).containsKey('error_message')) errorMessage = jsonDecode(response.body)['error_message'];
+      onError(errorMessage, response.statusCode);
+    }
+  }
+
+  void postItem(String endpoint, List<String> pathParams, Map<String, dynamic> bodyData, Function onSuccess, Function onError) async {
+    final uri = _buildUri(endpoint, pathParams, {});
+    final response = await http.post(uri, body: bodyData, headers: {
+      'Authorization': 'Bearer $_ACCESS_TOKEN',
+      'Content-Type': 'application/json'
+    });
+    if (response.statusCode == _UNAUTHORIZED) {
+      _refresh(() => postItem(endpoint, pathParams, bodyData, onSuccess, onError), () => _redirectToLogin());
+    } else if (response.statusCode ~/ 100 == 2) {
+      onSuccess(jsonDecode(response.body));
+    } else {
+      String errorMessage = 'No error message provided';
+      if (jsonDecode(response.body).containsKey('error_message')) errorMessage = jsonDecode(response.body)['error_message'];
+      onError(errorMessage, response.statusCode);
+    }
+  }
+
+  void putItem(String endpoint, List<String> pathParams, Map<String, dynamic> bodyData, Function onSuccess, Function onError) async {
+    final uri = _buildUri(endpoint, pathParams, {});
+    final response = await http.put(uri, body: bodyData, headers: {
+      'Authorization': 'Bearer $_ACCESS_TOKEN',
+      'Content-Type': 'application/json'
+    });
+    if (response.statusCode == _UNAUTHORIZED) {
+      _refresh(() => putItem(endpoint, pathParams, bodyData, onSuccess, onError), () => _redirectToLogin());
+    } else if (response.statusCode ~/ 100 == 2) {
+      onSuccess(jsonDecode(response.body));
+    } else {
+      String errorMessage = 'No error message provided';
+      if (jsonDecode(response.body).containsKey('error_message')) errorMessage = jsonDecode(response.body)['error_message'];
+      onError(errorMessage, response.statusCode);
+    }
+  }
+
+  void deleteItem(String endpoint, List<String> pathParams, Function onSuccess, Function onError) async {
+    final uri = _buildUri(endpoint, pathParams, {});
+    final response = await http.post(uri, headers: {
+      'Authorization': 'Bearer $_ACCESS_TOKEN',
+      'Content-Type': 'application/json'
+    });
+    if (response.statusCode == _UNAUTHORIZED) {
+      _refresh(() => deleteItem(endpoint, pathParams, onSuccess, onError), () => _redirectToLogin());
+    } else if (response.statusCode ~/ 100 == 2) {
+      onSuccess(jsonDecode(response.body));
+    } else {
+      String errorMessage = 'No error message provided';
+      if (jsonDecode(response.body).containsKey('error_message')) errorMessage = jsonDecode(response.body)['error_message'];
+      onError(errorMessage, response.statusCode);
+    }
+  }
+
+  void logOut() async {
+    _USER_ID = '';
+    _ACCESS_TOKEN = '';
+    _REFRESH_TOKEN = '';
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_REFRESH_TOKEN_PREFS);
+    _redirectToLogin();
+  }
+
+  void _refresh(Function onSuccess, Function onError) async {
+    // Llama a refresh, si es correcto setea las variables y llama a onSuccess. Si no llama a onError
+    final response = await http.get(API_URL+_REFRESH_ENDPOINT, headers: {
+      'Authorization': 'Bearer $_REFRESH_TOKEN',
+      'Content-Type': 'application/json'
+    });
+    if (response.statusCode ~/ 100 == 2) {
+      Map<String, String> credentials = jsonDecode(response.body);
+      _USER_ID = credentials['id'];
+      _ACCESS_TOKEN = credentials['access_token'];
+      if (credentials.containsKey('refresh_token')) {
+        _REFRESH_TOKEN = credentials['refresh_token'];
         final prefs = await SharedPreferences.getInstance();
-        await prefs.setString(_REFRESH_TOKEN_PREFS, REFRESH_TOKEN);
+        await prefs.setString(_REFRESH_TOKEN_PREFS, _REFRESH_TOKEN);
+      }
+      onSuccess();
+    } else {
+      onError();
     }
+  }
 
-    void tryInitializeFromPreferences() async {
-        // Esta función se llama al iniciar la aplicación. Determina si el usuario debe hacer login o si ya "se acuerda".
-        // Leer las preferences, buscar "socialout_refresh". Si no existe redirecciona a la screen de logIn
-        final prefs = await SharedPreferences.getInstance();
-        final String? refresh = prefs.getString(_REFRESH_TOKEN_PREFS);
-        if (refresh == null) redirectToLogin();
-        else {
-            // Si hay refresh token, iniciar sesión automáticamente llamando al endpoint de refresh de la API.
-            // Si la operación es aceptada redirecciona a la home screen. Si no redirecciona al logIn.
-            REFRESH_TOKEN = refresh;
-            refresh(() => redirectToHomeScreen(), () => redirectToLogin());
-        }
-    }
+  Uri _buildUri(String endpoint, List<String> pathParams, Map<String, String> queryParams) {
+    String formattedEndpoint = endpoint;
+    pathParams.forEachIndexed((idx, param) => formattedEndpoint = formattedEndpoint.replaceAll(":$idx", param));
+    return Uri.https(API_URL, formattedEndpoint, queryParams);
+  }
 
-    void getItem(String endpoint, List<String> pathParams, Function onSuccess, Function onError) async {
-        String formattedEndpoint = endpoint;
-        pathParams.forEachIndexed((idx, param) => formattedEndpoint = formattedEndpoint.replaceAll(":$idx", param));
-        final uri = Uri.https(API_URL, formattedEndpoint, {});
-        final response = await http.get(uri, headers: {
-            HttpHeaders.authorizationHeader: 'Bearer $ACCESS_TOKEN',
-            HttpHeaders.contentTypeHeader: 'application/json'
-        });
-        if (response.statusCode == UNAUTHORIZED) { // Intenta hacer refresh y relanzar la llamada a getItem
-            refresh(() => getItem(endpoint, pathParams, onSuccess, onError), () => redirectToLogin());
-        } else if (response.statusCode ~/ 100 == 2) {
-            onSuccess(jsonDecode(response.body));
-        } else {
-            onError(jsonDecode(response.body)['error_message'], response.statusCode);
-        }
-    }
+  void _redirectToLogin() {
+    // TODO: Navegar a la login screen
+  }
 
-    void getCollection(String endpoint, List<String> pathParams, Map<String, String> queryParams, Function onSuccess, Function onError) async {
-        String formattedEndpoint = endpoint;
-        pathParams.forEachIndexed((idx, param) => formattedEndpoint = formattedEndpoint.replaceAll(":$idx", param));
-        final uri = Uri.https(API_URL, formattedEndpoint, queryParams);
-        final response = await http.get(uri, headers: {
-            HttpHeaders.authorizationHeader: 'Bearer $ACCESS_TOKEN',
-            HttpHeaders.contentTypeHeader: 'application/json'
-        });
-        if (response.statusCode == UNAUTHORIZED) { // Intenta hacer refresh y relanzar la llamada a getCollection
-            refresh(() => getCollection(endpoint, pathParams, queryParams, onSuccess, onError), () => redirectToLogin());
-        } else if (response.statusCode ~/ 100 == 2) {
-            onSuccess(jsonDecode(response.body));
-        } else {
-            onError(jsonDecode(response.body)['error_message'], response.statusCode);
-        }
-    }
+  void _redirectToHomeScreen() {
+    // TODO: Navegar a la home screen
+  }
 
-    void postItem(String endpoint, List<String> pathParams, Map<String, dynamic> bodyData, Function onSuccess, Function onError) async {
+  factory APICalls() {
+    return _instance;
+  }
 
-    }
-
-    void putItem(String endpoint, List<String> pathParams, Map<String, dynamic> bodyData, Function onSuccess, Function onError) async {
-
-    }
-
-    void deleteItem(String endpoint, List<String> pathParams, Function onSuccess, Function onError) async {
-    
-    }
-
-    void logOut() async {
-        final prefs = await SharedPreferences.getInstance();
-        await prefs.remove(_REFRESH_TOKEN_PREFS);
-        redirectToLogin();
-    }
-
-    void refresh(Function onSuccess, Function onError) {
-        // Llama a refresh, si es correcto setea las variables y llama a onSuccess. Si no llama a onError
-        final response = await http.get(API_URL+REFRESH_ENDPOINT, headers: {
-            HttpHeaders.authorizationHeader: 'Bearer $REFRESH_TOKEN',
-            HttpHeaders.contentTypeHeader: 'application/json'
-        });
-        if (response.statusCode ~/ 100 == 2) {
-            Map<String, String> credentials = jsonDecode(response.body);
-            USER_ID = credentials['id'];
-            ACCESS_TOKEN = credentials['access_token'];
-            REFRESH_TOKEN = credentials['refresh_token'];
-            final prefs = await SharedPreferences.getInstance();
-            await prefs.setString(_REFRESH_TOKEN_PREFS, REFRESH_TOKEN);
-            onSuccess();
-        } else {
-            onError();
-        }
-    }
-
-    void redirectToLogin() {
-        // Navegar a la login screen
-    }
-
+  APICalls._internal();
 }
