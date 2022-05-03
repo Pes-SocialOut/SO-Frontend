1f00b4aae47aa8274722a9467ff23212e753ad4a
WeijieLiu1 <74435592+WeijieLiu1@users.noreply.github.com>
2022-05-03 17:49:34 +0200

    Update login_signUp.dart

--- lib/feature_user/services/login_signUp.dart
+++ lib/feature_user/services/login_signUp.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class userAPI {
-  final String urlToken = "https://socialout-develop.herokuapp.com/v1/users/";
+  final String basicUrl = "https://socialout-develop.herokuapp.com/v1/users/";
   String token = '';
   String refreshToken = '';
 
@@ -12,7 +12,9 @@ class userAPI {
   Future<List> checkUserEmail(email) async {
     String _path = 'register/check?type=socialout&email=';
 
-    final response = await http.get(Uri.parse(urlToken + _path + email));
+    String finalUri = basicUrl + _path + email;
+    print(finalUri);
+    final response = await http.get(Uri.parse(basicUrl + _path + email));
 
     if (response.statusCode != 200) {
       // return error
@@ -20,13 +22,51 @@ class userAPI {
     return json.decode(response.body);
   }
 
+  /* Comprobar que un Google socialout existe o no en la BD */
+  Future<List> checkUserGoogle(gtoken) async {
+    String _path = 'register/check?type=google&gtoken=';
+
+    final response = await http.get(Uri.parse(basicUrl + _path + gtoken));
+
+    if (response.statusCode != 200) {
+      // return error
+    }
+    return json.decode(response.body);
+  }
+
+    /* Comprobar que un Facebook socialout existe o no en la BD */
+  Future<List> checkUserFacebook(ftoken) async {
+    String _path = 'register/check?type=facebook&gtoken=';
+
+    final response = await http.get(Uri.parse(basicUrl + _path + ftoken));
+
+    if (response.statusCode != 200) {
+      // return error
+    }
+    return json.decode(response.body);
+  }
+
+  /* Get profile of a user given a access token */
+  Future<List> getDataAcessTokenGoogle(gtoken) async {
+    String googleProfileGetter = "https://www.googleapis.com/oauth2/v3/userinfo?access_token=";
+    
+
+    final response = await http.get(Uri.parse(googleProfileGetter + gtoken));
+
+    if (response.statusCode != 200) {
+      // return error
+    }
+    return json.decode(response.body);
+  }
+
+
   /*ultimo paso de registro*/
   Future<int> finalRegistrer(
       email, passw, username, description, languages, hobbies, codiVeri) async {
     String _path = 'register/socialout';
 
     final response = await http.post(
-      Uri.parse(urlToken + _path),
+      Uri.parse(basicUrl + _path),
       body: {
         "email": email,
         "password": passw,
@@ -39,7 +79,7 @@ class userAPI {
     );
 
     if (response.statusCode == 200) {
-      //access-tokken + id + refresh-token
+      token = response.body;
     }
     return response.statusCode;
   }
