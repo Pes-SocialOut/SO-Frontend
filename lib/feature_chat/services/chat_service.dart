dc363b9a58f21cce08dafd9bb72b5d221632cdb3
WeijieLiu1 <weijie.liu@estudiantat.upc.edu>
2022-05-24 17:19:23 +0200

    apichat

--- lib/feature_chat/services/chat_service.dart
+++ lib/feature_chat/services/chat_service.dart
@@ -6,19 +6,70 @@ class chatAPI {
   final String basicUrl = "https://socialout-develop.herokuapp.com/v1/users/";
 
   /* Crear el chat */
-  Future<Map<String, dynamic>> checkUserEmail(
-    String sender_id,
-    String chat_id,
-    String text
+  Future<http.Response> createChat(
+    String eventId,
+    String participantId,
     ) async {
     String _path = 'v1/chat/';
     String finalUri = basicUrl + _path;
     var str = {
-
+      "event_id": eventId,
+      "participant_id": participantId,
     };
 
+    final response = await http.post(Uri.parse(finalUri),
+        body: jsonEncode(str), headers: {'Content-Type': 'application/json'});
+
+    return response;
+  }
+
+  /* Crear el mensage */
+  Future<http.Response> createMessage(
+    String senderId,
+    String chatId,
+    String text,
+    ) async {
+    String _path = 'v1/chat/';
+    String finalUri = basicUrl + _path;
+    var str = {
+      "sender_id": senderId,
+      "chat_id": chatId,
+      "text": text,
+    };
+
+    final response = await http.post(Uri.parse(finalUri),
+        body: jsonEncode(str), headers: {'Content-Type': 'application/json'});
+
+    return response;
+  }
+
+  /* Eliminar el mensage */
+  Future<http.Response> deleteChat(
+    String eventId,
+    ) async {
+    String _path = 'v1/chat/';
+    String finalUri = basicUrl + _path;
+    var str = {
+      "event_id": eventId,
+    };
+
+    final response = await http.delete(Uri.parse(finalUri),
+        body: jsonEncode(str), headers: {'Content-Type': 'application/json'});
+
+    return response;
+  }
+  
+  /* Eliminar el mensage */
+  Future<http.Response> getListChat(
+    String creatorId,
+    ) async {
+    String _path = 'v1/chat/';
+    String finalUri = basicUrl + _path + creatorId;
+ 
+
     final response = await http.get(Uri.parse(finalUri));
-    return json.decode(response.body);
+
+    return response;
   }
 
 
