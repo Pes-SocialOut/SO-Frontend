1cd6d6e7c77fe144ed1a07faca12875bdb27604f
Guillem García <guillem.garcia.gil@protonmail.com>
2022-05-31 11:07:22 +0200

    HOTFIX: Merging issues with chat solved

--- lib/feature_user/services/signIn_google.dart
+++ lib/feature_user/services/signIn_google.dart
@@ -4,7 +4,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleSignInApi {
   //static final _clientID = '66737086171-2qao8l71itbh358cj1hqqbe5ughhjqun.apps.googleusercontent.com';
-  static final _googleSignIn = GoogleSignIn(clientId: _clientIDWeb);
+  static final _googleSignIn = GoogleSignIn(clientId: _clientIDWeb, scopes: <String>['email', 'https://www.googleapis.com/auth/calendar']);
   static const _clientIDWeb = '66737086171-n8tcu6ru19jja2sevcs99stj2ff2dn6g.apps.googleusercontent.com';
   static Future<GoogleSignInAccount?> login() => _googleSignIn.signIn();
   //https://accounts.google.com/o/oauth2/auth?scope=https://www.googleapis.com/auth/androidpublisher&response_type=code&access_type=offline&redirect_uri=http://localhost:8080&client_id=66737086171-n8tcu6ru19jja2sevcs99stj2ff2dn6g.apps.googleusercontent.com
